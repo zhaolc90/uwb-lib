774f555d53e3faa499a392fe49c619cfb414a838
c <ytmssr@outlook.com>
2018-06-17 17:04:33 +0800

    Toggle example

--- demo/src/pages/Toggle.js
+++ demo/src/pages/Toggle.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Toggle } from '../../../lib'
+import {Button} from 'react-bootstrap'
 
 const propTypes = {};
 
 const defaultProps = {};
 
 export default class TogglePage extends React.Component {
+    state = {
+        disabled: false
+    }
+    toggle = () => {
+        console.log(this.state.disabled)
+        this.setState({
+            disabled: !this.state.disabled
+        })
+    }
     render() {
+        const {disabled} = this.state
         return (
             <React.Fragment>
                 <h2>Toggle</h2>
-                <Toggle onClick={(e) => {console.log(e,'toggle clicked!')}}/>
-                <hr/>
+                <Toggle
+                    disabled={disabled}
+                    onClick={(e) => { console.log(e, 'toggle clicked!') }}
+                />
+                <hr />
+                <Button onClick={this.toggle}>tiggle disabled</Button>
             </React.Fragment>
         );
     }
